4b255b51592297e7325c82a74a95523510e85624
Habibullah <103850736+habibullah46@users.noreply.github.com>
2022-12-11 11:09:25 +0500

    Create hostel_management_using_ifelse.cpp

---
+++ hostel_management_using_ifelse.cpp
@@ -0,0 +1,225 @@
+
+#include<iostream>
+using namespace std;
+int a;
+int b;
+int R;
+int H;
+int T;
+void line()
+{
+	cout<<"==============================================\n";
+}
+
+int main()
+{
+char x;
+    do
+{     
+        
+		
+	cout<<"Hostel Name:\n"<<"Niyazi Boys hostel  \n"<<"Total Room:  6\n";
+	line();
+	cout<<"Press 8 to check 1st flour Rooms and 9 for 2nd flour Rooms\n";
+	cin>> b;
+	
+	if(b==8)
+	{
+	
+	
+    cout<<"In 1st floure there are 3 Rooms \n"<<"\nPress 1 to check Room number One\n"<<"\nPress 2 to check Room number two\n"<<"\nPress 3 to check Room number three\n";
+	line();
+	cin>>a;
+	
+	if (a==1)
+	{
+	
+	     cout<<"In Room No.1==>"<<"\n{ \n1) Habibullah, \n2) Muhammad Hasnain  \n3)Inayat Ali \n }\n"	;
+
+	line();
+        	cout<<"\nTo get details person in Room no 1\n"<<"\nPress 1\n ";
+	        cin>>R;
+	if(R==1)
+	      {
+		cout<<"Name: Habibullah\n"<<"CNIC: 71301-3516648-7\n"<<"Father Name: Abdul Khaliq\n"<<"CINC: 71301-2404784-9\n"<<"Address: Village Kunis, Tehsil Daghani ,District Ghanche, Gilgit Baltistan\n";
+		cout<<" Contact: 03408907705\n";
+	
+	line();
+	
+		cout<<"Name: Muhammad Hasnain\n"<<"CNIC: 71301-3316458-7\n"<<"Father Name: Razi Ali\n"<<"CINC: 71301-2546484-9\n"<<"Address: Village Partuk, Tehsil chorat ,District Ghanche, Gilgit Baltistan\n";
+		cout<<"Contact: 03445679456\n";
+	
+		line();
+		
+	
+		cout<<"Name:  Inayat Ali\n"<<"CNIC: 71301-5678349-7\n"<<"Father Name: Ghulam Hassan\n"<<"CINC: 71301-2424587-9\n"<<"Address: Village Marcha, Tehsil chorat ,District Ghanche, Gilgit Baltistan\n";
+		cout<<"Contact: 03147898765\n";
+	
+		line();
+      }}
+	
+		
+	if(a==2)
+	{
+	     cout<<"In Room No.2==>"<<"\n{\n1)Fida Muhammad, \n2) Waseem Balghari \n3) Raza Hari\n}";	
+	     
+	line();
+	        cout<<"\nTo get details person in Room no 2\n"<<"\nPress 2\n ";
+	        cin>>T;
+	if(T==2)
+	{
+		cout<<"Name: Fida Muhammad\n"<<"CNIC: 71301-2343546-7\n"<<"Father Name: Ghulam Mehdi\n"<<"CINC: 71301-7686987-5\n"<<"Address: Village Kunis, Tehsil Daghani ,District Ghanche, Gilgit Baltistan\n";
+		cout<<" Contact: 03407687995\n";
+    
+	line();
+	
+		cout<<"Name: Waseem Balghari\n"<<"CNIC: 71201-8956434-7\n"<<"Father Name: Abdul Baqi\n"<<"CINC: 71201-7865454-9\n"<<"Address: Village Balghari, Tehsil Daghani ,District Ghanche, Gilgit Baltistan\n";
+		cout<<"Contact: 03445679456\n";
+	
+		line();
+		
+	
+		cout<<"Name:  Raza Hari\n"<<"CNIC: 71301-9876786-7\n"<<"Father Name: Ghulam hussain\n"<<"CINC: 71301-9809786-9\n"<<"Address: Village Harikon, Tehsil Daghani ,District Ghanche, Gilgit Baltistan\n";
+		cout<<"Contact: 03147645365\n";
+	
+		line();
+	}
+}
+	if(a==3)
+	{
+		cout<<"In Room No.3==>"<<"\n{\n 1) Ejaz Ali, \n2) Syed Nayeem Shah \n3) Gulzar Hussain\n}";
+		
+	line();
+	        cout<<"\nTo get details person in Room no 3\n"<<"\nPress 3\n ";
+	        cin>>T;
+	if(T==3)
+	{
+		cout<<"Name: Ejaz Ali\n"<<"CNIC: 71301-9832543-7\n"<<"Father Name: Ghulam Muhammad\n"<<"CINC: 71301-8518990-9\n"<<"Address: Village Kalan, Tehsil chorbat ,District Ghanche, Gilgit Baltistan\n";
+		cout<<" Contact: 03554282562\n";
+    
+	line();
+	
+		cout<<"Name: Syed Nayeem shah\n"<<"CNIC: 71201-8956434-7\n"<<"Father Name: Syed Mumtaz\n"<<"CINC: 71201-7865454-9\n"<<"Address: Village Siksa, Tehsil chorbat ,District Ghanche, Gilgit Baltistan\n";
+		cout<<"Contact: 03405675456\n";
+	
+		line();
+		
+	
+		cout<<"Name:  Gulzar Hussain\n"<<"CNIC: 71301-2085348-6\n"<<"Father Name: Abdul Raheem\n"<<"CINC: 71301-9809786-9\n"<<"Address: Village siksah, Tehsil chorbat ,District Ghanche, Gilgit Baltistan\n";
+		cout<<"Contact: 03147645365\n";
+	
+		line();
+	  
+
+
+	}
+	}
+	
+}
+	
+	if(b==9)
+	{
+	
+	cout<<"In 2nd floure there are 3 Rooms\n "<<"\nPress 4 to check Room number One\n"<<"\nPress 5 to check Room number two\n"<<"\nPress 6 to check Room number three\n";
+	line();
+	cin>>a;
+	if(a==4)
+	{
+		cout<<"In Room No.4==>"<<"\n{\n1) Sanaullah,\n2) Tashfeen,\n3) Ammar,\n}";
+		
+	line();
+	        cout<<"\nTo get details person in Room no 4\n"<<"\nPress 4\n ";
+	        cin>>T;
+	if(T==4)
+	{
+		cout<<"Name:Sanaullah\n"<<"CNIC: 71301-2343546-7\n"<<"Father Name: Ghulam Mehdi\n"<<"CINC: 71301-7686987-5\n"<<"Address: Village Balghar, Tehsil Daghani ,District Ghanche, Gilgit Baltistan\n";
+		cout<<" Contact: 03407687995\n";
+    
+	line();
+	
+		cout<<"Name: Tashfeen\n"<<"CNIC: 71201-1034823-7\n"<<"Father Name: Abdullah \n"<<"CINC: 71201-7890868-9\n"<<"Address: Village Balghari, Tehsil Daghani ,District Ghanche, Gilgit Baltistan\n";
+		cout<<"Contact: 03445679456\n";
+	
+		line();
+		
+	
+		cout<<"Name:  Ammar\n"<<"CNIC: 71301-9876786-7\n"<<"Father Name: Ghulam hussain\n"<<"CINC: 71301-9809786-9\n"<<"Address: Village Harikon, Tehsil Daghani ,District Ghanche, Gilgit Baltistan\n";
+		cout<<"Contact: 03147645365\n";
+	
+		line();
+	  
+
+
+	}
+}
+	if(a==5)
+	{
+		cout<<"In Room No.5==>"<<"\n{\n1) Ismail ,\n2) Tajamul,\n3) Aziz\n}";
+		
+	line();
+	        cout<<"\nTo get details person in Room no 5\n"<<"\nPress 5\n ";
+	        cin>>T;
+	if(T==5)
+	{
+		cout<<"Name: Ismail\n"<<"CNIC: 71301-2343546-7\n"<<"Father Name: Ghulam Mehdi\n"<<"CINC: 71301-7686987-5\n"<<"Address: Village Kunis, Tehsil Daghani ,District Ghanche, Gilgit Baltistan\n";
+		cout<<" Contact: 03407687995\n";
+    
+	line();
+	
+		cout<<"Name: Tajamul\n"<<"CNIC: 71201-8956434-7\n"<<"Father Name: Nabi\n"<<"CINC: 71201-7865454-9\n"<<"Address: Village yougue, Tehsil Daghani ,District Ghanche, Gilgit Baltistan\n";
+		cout<<"Contact: 03445679456\n";
+	
+		line();
+		
+	
+		cout<<"Name:  Aziz\n"<<"CNIC: 71301-9876786-7\n"<<"Father Name: Ghulam hussain\n"<<"CINC: 71301-9809786-9\n"<<"Address: Village Harikon, Tehsil Daghani ,District Ghanche, Gilgit Baltistan\n";
+		cout<<"Contact: 03147645365\n";
+	
+		line();
+	  
+
+}
+	}
+	if(a==6)
+	{
+		cout<<"In Room No.6==>"<<"\n{\n1) Zulfiqar, \n2) Shoukat, \n3) Anwar\n}";
+		
+	line();
+	        cout<<"\nTo get details person in Room no 6\n"<<"\nPress 6\n ";
+	        cin>>T;
+	if(T==6)
+	{
+		cout<<"Name: Zulfiqar\n"<<"CNIC: 71301-2343546-7\n"<<"Father Name: Ali haider\n"<<"CINC: 71301-7686987-5\n"<<"Address: Village thaghas, Tehsil skardu ,District roudue, Gilgit Baltistan\n";
+		cout<<" Contact: 03407687995\n";
+    
+	line();
+	
+		cout<<"Name: Shoukat\n"<<"CNIC: 71201-8956434-7\n"<<"Father Name: Abdul Baqi\n"<<"CINC: 71201-7865454-9\n"<<"Address: Village Balghari, Tehsil Daghani ,District Ghanche, Gilgit Baltistan\n";
+		cout<<"Contact: 03445679456\n";
+	
+		line();
+		
+	
+		cout<<"Name:  Anwar\n"<<"CNIC: 71301-9876786-7\n"<<"Father Name: Ghulam hussain\n"<<"CINC: 71301-9809786-9\n"<<"Address: Village Harikon, Tehsil Daghani ,District Ghanche, Gilgit Baltistan\n";
+		cout<<"Contact: 03147645365\n";
+	
+		line();
+	  
+
+}
+	}
+	
+	
+	} 
+	 cout<<"Do u want to continue \n";
+	  cout<<"y or n";
+	 cin>>x;
+	 system("cls");
+	
+}while(x!='n');
+        
+
+}
+
+
+

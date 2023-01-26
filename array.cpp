eac71815a9bc9789254ed14f4a442ba5c67472ef
Habibullah <103850736+habibullah46@users.noreply.github.com>
2023-01-26 23:32:11 +0500

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   //Habibullah........
+   char b= 'c';
+   do
+   {
+   	int a;
+   	cout<<" enter number; ";
+   	cin>>a;
+   	cout<<a*10<<endl;
+   	int d=a*10;
+   
+   cout<<"press any char key to exit except of c"<<endl;
+   cin>>b;
+    cout<<d;
+}
+   while(b=='c');
+  
+}
